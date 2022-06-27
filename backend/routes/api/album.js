2f4b0b2f6cc02c4fdf9d29cf188e13f5b560861e
Attiya <adarensburg@live.com>
2022-06-27 17:41:38 -0500

    latest issue

--- backend/routes/api/album.js
+++ backend/routes/api/album.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const {Albums} = require('../../db/models/album');
+// const { check } = require('express-validator');
+
 
 const router = express.Router();
 
-const validator = [
-    check('title')
-        .exists({checkFalsy: true})
-        .withMessage('Please provide a title for your album')
-]
+// const validator = [
+//     check('title')
+//         .exists({checkFalsy: true})
+//         .withMessage('Please provide a title for your album')
+// ]
 
-router.post('/', validator, asyncHandler(async (req,res) =>{
+router.post('/', asyncHandler(async (req,res) =>{
     const {userId, title} = req.body;
     const newPhotoAlbum = await Albums.create({userId, title});
+    console.log(newPhotoAlbum);
     return res.json(newPhotoAlbum);
 }));
+
+module.export = router;
