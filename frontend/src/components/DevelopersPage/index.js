af82c5e97874a47a354fefc2d5d57dcaa566f262
Attiya <adarensburg@live.com>
2022-07-04 16:39:41 -0500

    ready for heroku

---
+++ frontend/src/components/DevelopersPage/index.js
@@ -0,0 +1,10 @@
+const DevelopersPage = () =>{
+    return(
+        <>
+        <h1>Developers</h1>
+            <p>Attiya Olivia Kovenburg</p>
+        </>
+    )
+};
+
+export default DevelopersPage;
---
+++ frontend/src/components/LanguagePage/index.js
@@ -0,0 +1,10 @@
+const LanguagePage = () =>{
+    return(
+        <>
+        <h1>Language</h1>
+            <p>English</p>
+        </>
+    )
+};
+
+export default LanguagePage;
