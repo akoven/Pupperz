8f5094853bd969f042b79d9ca2d4255c4e76ccf9
Attiya <adarensburg@live.com>
2022-07-27 13:11:42 -0500

    new login modal works

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
 import { NavLink, Link, Redirect } from "react-router-dom";
 import SplashPage from "../SplashPage";
 import './Navigation.css';
@@ -12,20 +13,21 @@ const Navigation = ({isLoaded}) =>{
     let sessionLinks;
     if(sessionUser){
         //try `/logged-in/${sessionUser.id}`
+        <Redirect to={`/logged-in/${sessionUser.id}`} />
         sessionLinks = (
             <>
                 <span className="title"><Link to={`/logged-in/${sessionUser.id}`}>Pupperz</Link></span>
                 <span className="albumSpan"><NavLink to={`/create-album/${sessionUser.id}`}>Create an Album</NavLink></span>
                 <ProfileButton user={sessionUser} />
-
             </>
         )
     }else{
         sessionLinks=(
             <>
                 <NavLink to='/' exact><div className="title">Pupperz</div></NavLink>
-                <span id="nav-button"><NavLink to='/login'><button className="nav-button">Log In</button></NavLink></span>
-                <span id="nav-button"><NavLink to='/signup'><button className="nav-button">Sign Up!</button></NavLink></span>
+                {/* <span id="nav-button"><NavLink to='/login'><button className="nav-button">Log In</button></NavLink></span> */}
+
+                <span id="nav-button"><LoginFormModal /><NavLink to='/signup'><button className="nav-button">Sign Up!</button></NavLink></span>
                 <SplashPage />
             </>
         )
--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -1,13 +1,38 @@
-import { Provider } from "react";
+import React, { useContext, useRef, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css';
 
-const ModalContext = () =>{
+const ModalContext = React.createContext();
 
-};
+export const ModalProvider = ({ children }) => {
+  const modalRef = useRef();
+  const [value, setValue] = useState();
 
-// export const ModalProvider = () =>{
-//     return{
-//         ModalContext.Provider()
-//     }
-// };
+  useEffect(() => {
+    setValue(modalRef.current);
+  }, [])
 
-export default ModalContext;
+  return (
+    <>
+      <ModalContext.Provider value={value}>
+        {children}
+      </ModalContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+}
+
+export const Modal = ({ onClose, children }) => {
+  const modalNode = useContext(ModalContext);
+  if (!modalNode) return null;
+
+  return ReactDOM.createPortal(
+    <div id="modal">
+      <div id="modal-background" onClick={onClose} />
+      <div id="modal-content">
+        {children}
+      </div>
+    </div>,
+    modalNode
+  );
+}
